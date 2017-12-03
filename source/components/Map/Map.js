c9e2e7cd0039d5ec1a6000b4ff2382fbd8a8f205
wlfjqcj <rhettqin@gmail.com>
2017-12-03 15:31:33 -0600

    submenu

--- source/components/Map/Map.js
+++ source/components/Map/Map.js
@@ -1,13 +1,41 @@
 import React, { Component } from 'react'
-import { Button, Menu } from 'semantic-ui-react'
+import ReactDOM from 'react-dom';
+import { Button } from 'semantic-ui-react'
 import { Link } from 'react-router-dom'
 import axios from 'axios'
 import GoogleMapReact from 'google-map-react';
 import GoogleMap from 'google-map-react';
+
+import Menu, { SubMenu, Item as MenuItem } from 'rc-menu';
+import createReactClass from 'create-react-class';
+
+import styles from './Map.scss';
+
 const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
 class SimpleMap extends Component {
+  onClick(info) {
+   console.log('click ', info);
+ }
 
+  getMenu() {
+    return (
+      <Menu
+        onClick={this.onClick}
+        mode="inline"
+      >
+
+        <SubMenu key="1" title="Menu">
+          <MenuItem key="1-1">LogIn</MenuItem>
+          <MenuItem key="1-2">Register</MenuItem>
+          <MenuItem key="1-3">New Chat</MenuItem>
+          <MenuItem key="1-4">New Event</MenuItem>
+          <MenuItem key="1-5">New Helper</MenuItem>
+        </SubMenu>
+
+      </Menu>
+    );
+  }
 
   static get defaultProps() {
      return {
@@ -15,20 +43,30 @@ class SimpleMap extends Component {
          zoom: 8
      }
    }
+
   render() {
     return (
-        <div style = {{ height: '100px' , width : '100px'}}>
-      <GoogleMapReact style = {{ height: '100px' , width : '100px'}}
-        defaultCenter={this.props.center}
-        defaultZoom={this.props.zoom}
-      >
-        <AnyReactComponent
-          lat={11}
-          lng={22}
-          text={'Kreyser Avrora'}
-        />
-      </GoogleMapReact>
+    <div className = 'wrapper'>
+
+
+      <div style = {{ height: '100px' , width : '100px'}}>
+        <GoogleMapReact style = {{ height: '100px' , width : '100px'}}
+          defaultCenter={this.props.center}
+          defaultZoom={this.props.zoom}
+        >
+          <AnyReactComponent
+            lat={11}
+            lng={22}
+            text={'Kreyser Avrora'}
+          />
+        </GoogleMapReact>
       </div>
+
+      <div className = 'submenu'>{this.getMenu()}</div>
+    </div>
+
+
+
     );
   }
 }
